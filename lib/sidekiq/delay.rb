933d2e44dae935ab5a63a9fab4f57ea68fb7320b
Daniel Libanori <daniellibanori@gmail.com>
2013-06-16 22:23:33 -0300

    Initial commit

---
+++ lib/sidekiq/delay.rb
@@ -0,0 +1,7 @@
+require "sidekiq/delay/version"
+
+module Sidekiq
+  module Delay
+    # Your code goes here...
+  end
+end
---
+++ lib/sidekiq/delay/version.rb
@@ -0,0 +1,5 @@
+module Sidekiq
+  module Delay
+    VERSION = "0.0.1"
+  end
+end
