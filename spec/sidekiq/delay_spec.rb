804f3261f877e18d2127f905f73486b25bacf83b
Daniel Libanori <daniellibanori@gmail.com>
2013-06-22 19:16:53 -0300

    Improve spec covarage

--- spec/sidekiq/delay_spec.rb
+++ spec/sidekiq/delay_spec.rb
@@ -1,14 +1,17 @@
 require "spec_helper"
 require 'sidekiq/testing'
-
 require 'app/models/band'
 require 'app/models/team'
 
 describe Sidekiq::Delay do
   context "with default worker" do
+    subject { Band }
     let(:band) { Band.new }
+    let(:worker) { Sidekiq::Delay::DefaultWorker }
 
-    it "queue an job at default queue" do
+    its(:worker) { should eq(worker) }
+
+    it "queues a job at default queue" do
       expect {
         band.delay.play
       }.to change(Sidekiq::Delay::DefaultWorker.jobs, :size).by(1)
@@ -16,9 +19,13 @@ describe Sidekiq::Delay do
   end
 
   context "with custom worker" do
+    subject { Team }
     let(:team) { Team.new }
+    let(:worker) { TeamWorker }
 
-    it "queue an job at default queue" do
+    its(:worker) { should eq(worker) }
+
+    it "queues a job at custom queue" do
       expect {
         team.delay.play
       }.to change(TeamWorker.jobs, :size).by(1)
